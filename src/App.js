d90f27428f1e6d44f45848f522f634c7dffdcfb9
Nhan Tran <trannhan@usc.edu>
2018-08-09 20:57:28 -0700

    got new line of text to work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./css/App.css";
+import uid from "uid";
 
 const data = {
   experience: ["J2 Global", "USC Interaction Lab"],
@@ -12,7 +13,8 @@ class App extends Component {
     super(props);
     this.captureText = this.captureText.bind(this);
     this.state = {
-      textLine: [],
+      textLine: [{ text: "", blink: true }],
+      count: 0,
       text: "$ ",
       upper: false
     };
@@ -20,6 +22,12 @@ class App extends Component {
 
   componentDidMount() {
     window.location.hash = "#maintab";
+    var textLine = this.state.textLine;
+    textLine[0].text = this.state.text;
+    textLine[0].blink = true;
+    this.setState({
+      textLine: textLine
+    });
   }
 
   captureText(e) {
@@ -29,8 +37,11 @@ class App extends Component {
       for (var i = 0; i < str.length - 1; i++) {
         returnVal += str[i];
       }
+      var textLine = this.state.textLine;
+      textLine[this.state.count].text = returnVal;
       this.setState({
-        text: returnVal
+        text: returnVal,
+        textLine: textLine
       });
     } else if (e.key === "Shift") {
       this.setState({
@@ -39,19 +50,31 @@ class App extends Component {
     } else if (e.key === "Control") {
     } else if (e.key === "Alt") {
     } else if (e.key === "Enter") {
-      document.getElementById("ls").style.visibility = "visible";
+      var count = this.state.count + 1;
+      var textLine = this.state.textLine;
+      var add = { text: "$ ", blink: true };
+      textLine.push(add);
+      textLine[count - 1].blink = false;
+      this.setState({
+        textLine: textLine,
+        text: "$ ",
+        count: this.state.count + 1
+      });
     } else {
       var text = this.state.text + e.key;
-      var textLine = this.state.text.push(text);
+      var textLine2 = this.state.textLine;
+      textLine2[this.state.count].text = text;
       this.setState({
         text: text,
-        textLine: textLine
+        textLine: textLine2
       });
     }
   }
 
   render() {
     console.log(this.state.text);
+    console.log(this.state.textLine);
+
     return (
       <div className="App">
         <div className="box">
@@ -76,16 +99,14 @@ class App extends Component {
             id="maintab"
           >
             <div className="displayText">
-              {this.state.text}
-              <span className="blinking-cursor">|</span>
-            </div>
-            <div className="ls" id="ls">
-              <ul>
-                <li>Projects</li>
-                <li>Experience</li>
-                <li>Skill</li>
-                <li>About</li>
-              </ul>
+              {this.state.textLine.map(el => {
+                return (
+                  <div>
+                    <p key={uid()}>{el.text}</p>
+                    {el.blink && <span className="blinking-cursor">|</span>}
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
